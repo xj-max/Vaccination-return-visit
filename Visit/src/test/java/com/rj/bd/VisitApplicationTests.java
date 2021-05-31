bbea6ea59e93c405a418221076dd54290edd2047
xj-max <76678351+xj-max@users.noreply.github.com>
2021-05-31 20:27:14 +0800

    Add files via upload

---
+++ Visit/src/test/java/com/rj/bd/VisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rj.bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
